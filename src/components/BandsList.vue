a9692d169570430c70a7b853cf1981ee02aea32e
Dhriti5 <dhritiarora75@gmail.com>
2018-11-13 12:52:12 +0530

    Added the band

--- src/components/BandsList.vue
+++ src/components/BandsList.vue
@@ -1,37 +1,49 @@
 <template>
-  <div>
-    <input type="text" placeholder="Add Band..">
-    <button> Add </button>
-
-    <ul>
-      <BandItem v-for="band in bands" :key="band.id" :band="band">
-        {{band.name}}
-      </BandItem>
-    </ul>
-  </div>
+    <div>
+    <input type="text" placeholder="Add band..." v-model="newBand"></input>
+    <button @click="addBand">Add</button>
+        <ul>
+            <BandItem v-for="band in bands"  v-bind:band="band">
+            </BandItem>
+        </ul>
+    </div>
 </template>
-
 
 <script>
 import BandItem from '@/components/BandItem.vue'
 
-export default {
-  name: 'BandList',
-  components: {
+export default{
+name: 'BandList',
+ components: {
     BandItem
   },
-  data () {
-    return {
-      bands: [{
-        id: 1,
-        name: 'Black Sabbath',
-        songs: ['Iron Man', 'War Pigs']
-      }, {
-        id: 2,
-        name: 'ColdPlay',
-        songs: ['Yellow', 'Fix you', 'Mylo xyloto']
-      }]
+    data(){
+    return{
+        index : 2,
+        bands:[
+            {
+                id: 1,
+                name:'Black Sabbath',
+                songs:['Paranoid', 'into the void']
+            },{
+                id: 2,
+                name: 'Maroon 5',
+                 songs:['Sugar', 'Girls like you']
+            }
+        ]
     }
-  }
+},
+methods:{
+    addBand(){
+        this.index++;
+        var bandItem = {id: this.index,
+        name: this.newBand,
+        songs: []
+        }
+        this.bands.push(bandItem)
+        this.newBand=''
+    }
+}
+
 }
 </script>
